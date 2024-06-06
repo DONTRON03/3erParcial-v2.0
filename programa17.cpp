2789610a0207cf767aaa168420f7ce2a46f9d7ae
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-06-06 11:21:21 -0600

    real programa 17

---
+++ programa17.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//Numeros del 1 al 100, de 5 en 5
+int main() {
+    for(int i = 5; i <= 100; i+=5) {
+        cout << i << " ,";
+    }
+
+    return 0;
+}
+
+
