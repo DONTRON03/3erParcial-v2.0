62c217e1a525fc7c07ba492f3c5526582ff27d91
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-06-14 00:45:50 -0600

    Update programa43.cpp

--- programa43.cpp
+++ programa43.cpp
@@ -1,57 +1,12 @@
-// Actividad 13_ 27 de mayo del 2024 _ 3er parcial
-//Modificacion con matrices
 #include <iostream>
 
 int main() {
-	
-    int sumf = 0;
-    int sumf2 = 0;
-    int f;
-    std::cout << "Digite el número de filas: ";
-    std::cin >> f;
-    int c;
-    std::cout << "Digite el número de columnas: ";
-    std::cin >> c;
-    int A[f][c],B[f][c],C[f][c];
-
-    //INGRESO DE DATOS Y SUMA DE ELEMENTOS
-    std::cout<<"Matriz A:  \n";
-    for (int i = 0; i < f; ++i) {
-        for (int j = 0; j < c; ++j) {
-            std::cout << "Digite dato para la fila " << i << " columna " << j << ": ";
-            std::cin >> A[i][j];
-            sumf += A[i][j];
+	 cout << "Matriz:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << rand() % 10 << " ";
         }
-    }
-	std::cout << "La suma de todos los elementos de la columna es: " << sumf << std::endl;
-	std::cout<< "\n ";
-
-    std::cout<<"Matriz B:  \n";
-    for (int i = 0; i < f; ++i) {
-        for (int j = 0; j < c; ++j) {
-            std::cout << "Digite dato para la fila " << i << " columna " << j << ": ";
-            std::cin >> B[i][j];
-            sunf2 += B[i][j];
-        }
-    }
-	std::cout << "La suma de todos los elementos de la columna es: " << sumf << std::endl;
-	std::cout<< "\n ";
-
-    //DATOS DE LAS MATRICES
-    std::cout << "Los datos que hay en la matriz  A son:" << std::endl;
-    for (int i = 0; i < f; ++i) {
-        for (int j = 0; j < c; ++j) {
-            std::cout << A[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-	    std::cout << "Los datos que hay en la matriz B son:" << std::endl;
-    for (int i = 0; i < f; ++i) {
-        for (int j = 0; j < c; ++j) {
-            std::cout << B[i][j] << " ";
-        }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
 
